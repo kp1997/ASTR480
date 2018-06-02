935b25b8c6f4379a0227d8fa51b29a34dbedf966
kp1997 <kyle.porter1997@gmail.com>
2018-06-01 18:01:19 -0700

    Complete sci-kit-learn exercise and add more final codes

---
+++ Sci-Kit-Learn- ML-Lab/exercise_02_sentiment.py
@@ -0,0 +1,109 @@
+"""Build a sentiment analysis / polarity model
+
+Sentiment analysis can be casted as a binary text classification problem,
+that is fitting a linear classifier on features extracted from the text
+of the user messages so as to guess wether the opinion of the author is
+positive or negative.
+
+In this examples we will use a movie review dataset.
+
+"""
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
+
+import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import numpy as np
+
+
+if __name__ == "__main__":
+    # NOTE: we put the following in a 'if __name__ == "__main__"' protected
+    # block to be able to use a multi-core grid search that also works under
+    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
+    # The multiprocessing module is used as the backend of joblib.Parallel
+    # that is used when n_jobs != 1 in GridSearchCV
+
+    # the training data folder must be passed as first argument
+    movie_reviews_data_folder = sys.argv[1]
+    dataset = load_files(movie_reviews_data_folder, shuffle=False)
+    print("n_samples: %d" % len(dataset.data))
+
+    # split the dataset in training and test set:
+    docs_train, docs_test, y_train, y_test = train_test_split(
+        dataset.data, dataset.target, test_size=0.25, random_state=None)
+
+    # TASK: Build a vectorizer / classifier pipeline that filters out tokens
+    # that are too rare or too frequent
+    from sklearn.feature_extraction.text import CountVectorizer
+    count_vect = CountVectorizer()
+
+    tfidf_transformer = TfidfTransformer()
+    X_train_counts = count_vect.fit_transform(dataset.data)
+    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    X_train_tfidf.shape
+
+    from sklearn.naive_bayes import MultinomialNB
+
+    text_clf = Pipeline([('vect', CountVectorizer()),
+                        ('clf', MultinomialNB())])
+
+
+    from sklearn.linear_model import SGDClassifier
+
+
+    text_clf.fit(dataset.data, dataset.target) 
+
+    predicted = text_clf.predict(docs_test)
+
+    # TASK: Build a grid search to find out whether unigrams or bigrams are
+    # more useful.
+    # Fit the pipeline on the training set using grid search for the parameters
+    parameters = {'vect__ngram_range': [(1, 1), (1, 2)],
+    }
+
+    gs_clf = GridSearchCV(text_clf, parameters, n_jobs=-1)
+    gs_clf = gs_clf.fit(docs_train, y_train)
+
+    # TASK: print the cross-validated scores for the each parameters set
+    # explored by the grid search
+    n_candidates = len(gs_clf.cv_results_['params'])
+    for i in range(n_candidates):
+        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
+                 % (gs_clf.cv_results_['params'][i],
+                    gs_clf.cv_results_['mean_test_score'][i],
+                    gs_clf.cv_results_['std_test_score'][i]))
+
+
+    # TASK: Predict the outcome on the testing set and store it in a variable
+    # named y_predicted
+    from sklearn.naive_bayes import MultinomialNB
+    #clf = MultinomialNB().fit(X_train_tfidf, docs_train.target)
+
+    X_new_counts = count_vect.transform(dataset.data)
+    X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+
+
+
+    #y_predicted = text_clf.predict(X_train_tfidf)
+    y_predicted = gs_clf.predict(docs_test)
+
+
+    # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
+
+    # Print and plot the confusion matrix
+    cm = metrics.confusion_matrix(y_test, y_predicted)
+    print(cm)
+
+    # import matplotlib.pyplot as plt
+    # plt.matshow(cm)
+    # plt.show()
